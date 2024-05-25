4fa3bcb058d771a0ba3357087f366ca1d1c08c4f
nwasir <uko20001@byui.edu>
2024-05-25 15:36:26 +0100

    update database

---
+++ database/assignment2.sql
@@ -0,0 +1,40 @@
+--insert into account
+INSERT INTO public.account
+	(account_firstname, account_lastname, account_email, account_password)
+VALUES
+	('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n')
+
+--update account
+UPDATE 
+	public.account
+SET 
+	account_type = 'Admin'
+WHERE 
+	account_id = 1
+
+-- delete 
+DELETE FROM public.account
+WHERE
+	account_id = 1
+
+-- update
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10
+
+-- inner join
+SELECT 
+	inv_make,
+	inv_model,
+	classification_name
+FROM
+	public.inventory
+INNER JOIN public.classification
+	ON public.inventory.classification_id = public.classification.classification_id
+ WHERE classification_name = 'Sport'
+
+--replace
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+	
--- database/database_code.sql
+++ database/database_code.sql
@@ -237,3 +237,14 @@ VALUES   (
     'White',
     5
   );
+
+-- update
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10
+
+--replace
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+	
